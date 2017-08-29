3293ade188009654d9c185ad1af4de5311927b98
liuy1994 <liuy1994@outlook.com>
2017-08-29 19:46:17 +0800

    add jquery

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ $('ol').on('click','li',function(e){
     let $li = $(e.currentTarget)    
     $li.addClass('active').siblings().removeClass('active')
     let index = $li.index()
-    console.log($('tabs-content'))
     $('.tabs-content').children().eq(index).addClass('active').siblings().removeClass('active')
 })
 
