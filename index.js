23c971abc9ee399103d47ac7caada1831a70348a
liuy1994 <liuy1994@outlook.com>
2017-08-30 06:21:46 +0800

    add search

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
 // tab组件
-$('ol').on('click','li',function(e){
-    let $li = $(e.currentTarget)    
+$('ol#tabs').on('click', 'li', function (e) {
+    let $li = $(e.currentTarget)
     $li.addClass('active').siblings().removeClass('active')
     let index = $li.index()
-    $('.tabs-content').children().eq(index).addClass('active').siblings().removeClass('active')
+    $('#tabs-content').children().eq(index).addClass('active').siblings().removeClass('active')
 })
 
 // 清空搜索框
-$('.icon-empty').on('click',function(e){
-    $(e.currentTarget).parent().children('input')[0].value = '';
-    $($('.note')[0]).removeClass('input');
+$('#icon-empty').on('click', function (e) {
+    $(e.currentTarget).parent().children('input#search')[0].value = '';
+    $($('#note')[0]).removeClass('input');
+    $($('#icon-empty')[0]).removeClass('input');
 })
 
 // 搜索框默认文字清空
-function search(){
-    $($('.note')[0]).addClass('input');
-    if($('input')[0].value === ''){
-        $($('.note')[0]).removeClass('input');
+$('input#search').on('input', function (e) {
+    $($('#note')[0]).addClass('input');
+    $($('#icon-empty')[0]).addClass('input');
+    if ($('input#search')[0].value === '') {
+        $($('#note')[0]).removeClass('input');
+        $($('#icon-empty')[0]).removeClass('input');
     }
-}
-
-// 快速热门搜索
-$('ul.list').on('click','li',function(e){
-    $('input')[0].value = e.currentTarget.innerText;
-    
-    $($('.note')[0]).addClass('input');
 })
+// 快速热门搜索
+$('ul#search-hot').on('click', 'li', function (e) {
+    $('input#search')[0].value = e.currentTarget.innerText;
+    $($('#icon-empty')[0]).addClass('input');
+    $($('#note')[0]).addClass('input');
+    var value = $('input#search').val().trim();
+    search(value);
+})
+
+// 监听搜索框输入的内容
+$('input#search').on('input', function (e) {
+    var value = $(e.currentTarget).val().trim();
+    search(value);
+})
+function search(value) {
+    if(value === ''){
+    }else{
+        $('#search-result').empty()
+        var query = new AV.Query('Song');
+        query.contains('name', value);
+        query.find().then(function (result) {
+            for(var i=0;i<result.length;i++){
+                var song = result[i].attributes;
+                var a = `
+                <a href="#" data-id="${result[i].id}">
+                <div class="a">
+                    <div class="name">
+                        <p>${song.name}</p>
+                    </div>
+                    <div class="author sq">${song.author} - ${song.album}</div>
+                    <div class="icon-play"></div>
+                </div>
+            </a>
+                `
+                $('#search-result').append(a);
+            }
+            
+        })
+    }
+    
+}
