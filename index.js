a953734e25e69a697d856be9294dcb610b7202c3
liuy1994 <liuy1994@outlook.com>
2017-08-30 20:53:12 +0800

    llll

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ $('#icon-empty').on('click', function (e) {
     $(e.currentTarget).parent().children('input#search')[0].value = '';
     $($('#note')[0]).removeClass('input');
     $($('#icon-empty')[0]).removeClass('input');
+    $($('.search-hot')[0]).removeClass('input');        
+    $('#search-content').empty();
+    $($('#search-content')[0]).addClass('input');
+    $($('.search-hot')[0]).removeClass('input');        
+    $('#search-content').empty();
+    $($('#search-content')[0]).addClass('input');
+    $('#search-result').empty();
+    $('#result-item').empty();
+    $($('.search-hot')[0]).removeClass('input');
 })
 
 // 搜索框默认文字清空
@@ -20,6 +29,14 @@ $('input#search').on('input', function (e) {
     if ($('input#search')[0].value === '') {
         $($('#note')[0]).removeClass('input');
         $($('#icon-empty')[0]).removeClass('input');
+        $($('.search-hot')[0]).removeClass('input');        
+        $('#search-content').empty();
+        $($('#search-content')[0]).addClass('input');
+        $($('.search-hot')[0]).removeClass('input');        
+        $('#search-content').empty();
+        $($('#search-content')[0]).addClass('input');
+        $('#result-item').empty();
+        $($('.search-hot')[0]).removeClass('input');
     }
 })
 // 快速热门搜索
@@ -27,6 +44,7 @@ $('ul#search-hot').on('click', 'li', function (e) {
     $('input#search')[0].value = e.currentTarget.innerText;
     $($('#icon-empty')[0]).addClass('input');
     $($('#note')[0]).addClass('input');
+    $($('.search-hot')[0]).addClass('input');
     var value = $('input#search').val().trim();
     searchName(value);
     searchAuthor(value);
@@ -35,20 +53,27 @@ $('ul#search-hot').on('click', 'li', function (e) {
 // 监听搜索框输入的内容
 $('input#search').on('input', function (e) {
     var value = $(e.currentTarget).val().trim();
+    $($('.search-hot')[0]).addClass('input');
     searchName(value);
     searchAuthor(value);
+    if($('#search-result')[0].innerText === ''){
+        $('#search-result')[0].innerText === '没有结果'
+    }
 })
 
 //搜索歌曲
 function searchName(value) {
+    $('#search-result').empty()
     if(value === ''){
+
     }else{
-        $('#search-result').empty()
+        
         var query = new AV.Query('Song');
         query.contains('name', value);
         query.find().then(function (result) {
             for(var i=0;i<result.length;i++){
                 var song = result[i].attributes;
+                
                 var a = `
                 <a href="#" data-id="${result[i].id}">
                 <div class="a">
@@ -69,9 +94,16 @@ function searchName(value) {
 }
 //搜索歌手
 function searchAuthor(value) {
+    $('#search-result').empty()
     if(value === ''){
+        
     }else{
-        $('#search-result').empty()
+        $($('#result-item')[0]).removeClass('input')
+        $($('#search-content')[0]).removeClass('input');
+        var p = `
+        <p>搜索“${value}”</p>
+        `
+        $('#search-content').append(p);
         var query = new AV.Query('Song');
         query.contains('author', value);
         query.find().then(function (result) {
